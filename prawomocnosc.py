b388dfe7d0047fb79598c2987394046b797279f0
czeroooo <130399470+czeroooo@users.noreply.github.com>
2023-04-12 08:44:11 +0200

    Update prawomocnosc.py

--- prawomocnosc.py
+++ prawomocnosc.py
@@ -8,6 +8,8 @@ def data_prawomocnosci(dzien_doreczenia, miesiac_doreczenia, rok_doreczenia, typ
       okres_prawomocnosci = 30
   elif typ_orzeczenia == 'postanowienie':
       okres_prawomocnosci = 7
+  elif typ_orzeczenia == 'decyzja'
+      okres_prawomocnosci = 14
   else:
       raise ValueError('Nieznany typ orzeczenia')
   data_prawomocnosci = data_doreczenia + datetime.timedelta(days=okres_prawomocnosci)
