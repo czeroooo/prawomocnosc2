577e00ee64b43076be4df90352296134c22beacf
czeroooo <130399470+czeroooo@users.noreply.github.com>
2023-04-12 10:21:20 +0200

    Update prawomocnosc.py

--- prawomocnosc.py
+++ prawomocnosc.py
@@ -29,7 +29,7 @@ def data_prawomocnosci(data_doreczenia, typ_orzeczenia):
           break
   return data_prawomocnosci
 
-st.title("Obliczanie dat prawomocności orzeczeń")
+st.title("Ustalanie prawomocności orzeczeń")
 data_doreczenia = st.date_input("Podaj datę doręczenia", value=datetime.date(2023, 1, 1), min_value=datetime.date(1900, 1, 1), max_value=datetime.date(2100,12,31))
 typ_orzeczenia = st.selectbox("Typ orzeczenia", ["wyrok", "postanowienie", "decyzja II instancji", "decyzja II instancji - art. 138 § 2", "decyzja I instancji"])
 
