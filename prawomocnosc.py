3d537eada3f60e2bc4981acde6a8159a4875ff46
czeroooo <130399470+czeroooo@users.noreply.github.com>
2023-04-19 11:46:34 +0200

    Update prawomocnosc.py

--- prawomocnosc.py
+++ prawomocnosc.py
@@ -35,4 +35,4 @@ data_doreczenia = st.date_input("Podaj datę doręczenia", value=datetime.date(2
 data_prawomocnosci = data_prawomocnosci(data_doreczenia, typ_orzeczenia)
 
 st.write("Data prawomocności orzeczenia: ", data_prawomocnosci)
-st.caption('Zaprojektował: Michał Czerniak')
+st.caption('Zaprojektował: Michał Czerniak - WSA Poznań')
