4190d0a7823790df90c0b0caaf36c6c410ee69cc
czeroooo <130399470+czeroooo@users.noreply.github.com>
2025-08-19 09:32:09 +0200

    Update prawomocnosc.py

--- prawomocnosc.py
+++ prawomocnosc.py
@@ -33,12 +33,13 @@ def data_prawomocnosci(data_doreczenia, typ_orzeczenia):
 
 st.title("Obliczanie prawomocności orzeczeń")
 typ_orzeczenia = st.selectbox("Typ orzeczenia", ["wyrok", "postanowienie", "decyzja II instancji", "decyzja II instancji - art. 138 § 2", "decyzja I instancji"])
-data_doreczenia = st.date_input("Podaj datę doręczenia", value=datetime.date(2023, 1, 1), min_value=datetime.date(1900, 1, 1), max_value=datetime.date(2100,12,31))
+data_doreczenia = st.date_input("Podaj datę doręczenia", value=datetime.date(2024, 1, 1), min_value=datetime.date(1900, 1, 1), max_value=datetime.date(2100,12,31))
 data_prawomocnosci = data_prawomocnosci(data_doreczenia, typ_orzeczenia)
 
 st.markdown(f"<span style='font-size: 20px; color: white;'>Data prawomocności orzeczenia: </span><span style='font-size: 30px; color: green;'>{data_prawomocnosci}</span>", unsafe_allow_html=True)
 
 st.caption('Zaprojektował: Michał Czerniak - WSA Poznań')
+
 
 
 
