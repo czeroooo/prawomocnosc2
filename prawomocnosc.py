5831f07f14e7a0458b68b9a66077392d758b583d
czeroooo <130399470+czeroooo@users.noreply.github.com>
2026-03-04 11:13:21 +0100

    Update prawomocnosc.py

--- prawomocnosc.py
+++ prawomocnosc.py
@@ -24,7 +24,7 @@ def data_prawomocnosci(data_doreczenia, typ_orzeczenia):
   polskie_swieto = holidays.Polish()
 
   while True:
-    # 1. wyliczamy ostatni dzień terminu (nie wliczamy dnia doręczenia)
+# 1. wyliczamy ostatni dzień terminu (nie wliczamy dnia doręczenia)
 ostatni_dzien = data_doreczenia + datetime.timedelta(days=okres_prawomocnosci)
 
 polskie_swieto = holidays.Polish()
@@ -47,6 +47,7 @@ data_prawomocnosci = data_prawomocnosci(data_doreczenia, typ_orzeczenia)
 st.markdown(f"<span style='font-size: 20px; color: white;'>Data prawomocności orzeczenia: </span><span style='font-size: 30px; color: green;'>{data_prawomocnosci}</span>", unsafe_allow_html=True)
 
 st.caption('Zaprojektował: Michał Czerniak - Wojewódzki Sąd Administracyjny w Poznaniu')
+
 
 
 
